2d0b23c51cbd95c19a589fe7895110e74d31313d
Vergil Weatherford <vergil.weatherford@navigant.com>
2017-05-02 08:50:54 -0400

    First commit including comment outline of overall approach. Includes raw data.

---
+++ run_analysis.R
@@ -0,0 +1,29 @@
+#/******************************************************************************
+# SCRIPT NAME: run_analysis.R
+# PROJECT: Coursera "Gettinga and Cleaning Data" final project
+# DESCRIPTION: Load and clean "wearable tech" sensor data taken from a Samsung Galaxy S smartphone.
+# PROGRAMMER: Vergil Weatherford
+# DATE: 2017-05-02
+# GIT: https://github.com/vergilcw/coursera_wearable
+# INPUT PATH: input/
+# OUTPUT PATH: output/
+# SECTIONS:
+#   A - Read and Clean Raw Data
+#   B - Merge the training and the test sets to create one data set.
+#   C - Extract only the measurements on the mean and sd for each measurement.
+#   D - Use descriptive activity names to name the activities in the data set.
+#   E - Label the data set with descriptive variable names.
+#   F - Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+#******************************************************************************/
+
+
+# A - Read Raw Data -------------------------------------------------------
+
+
+
+# B - Clean Data ----------------------------------------------------------
+
+
+# C - Export Cleaned CSVs -------------------------------------------------
+
+
